50f841bb62a50fd8ae37405ea7fe32a338d806f4
Jose Guillermo Garcia Serrano <joseg1224@hotmail.com>
2018-06-26 20:26:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,27 @@ var meals = {};
 var meals = new Object();
 var meals = { breakfast: "oatmeal"};
 var meals = new Object({breakgast: "oatmeal"});
+const firstMeal = "breakfast";
+var meals = {firstMeal: "oatmeal"};
+//objects key would be "firstmeal", not "breakgast"
+var meals = {[firstMeal]: "oatmeal"}
+//objects key would be "breakfast"
+//dot notation
+meals.breakfast // "oatmeal"
+meals["breakfast"] //"oatmeal"
+meals[firstMeal] //"oatmeal"
+meals.firstMeal //undefined
+var meals = {
+  breakfast: "oatmeal",
+  lunch: "burrito",
+  dinner: "steak"
+}
+meals.snack = "yogurt";
+meals.snack //"yogurt"
+meals["snack"]//"yogurt"
+meals.lunch //"burrito"
+meals["second breakfast"] = "bagel"
+var sweetMeal = "dessert"
+meals[sweetMeal] = "cake";
+meals.dessert // "cake"
+meals[sweetMeal] // "cake"
